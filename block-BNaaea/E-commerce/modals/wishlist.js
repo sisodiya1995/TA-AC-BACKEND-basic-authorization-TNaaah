e12c52bda9b59da99023e4892a2eb844084e6703
Akash Singh <akashingh95@gmail.com>
2022-04-27 21:34:28 +0530

    E-commerce|express

---
+++ block-BNaaea/E-commerce/modals/wishlist.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+var schema = mongoose.Schema;
+var slug = require('mongoose-slug-generator');
+mongoose.plugin(slug);
+
+var wishSchema = new schema({
+    title : {type : String , required : true} ,
+    quantity : {type : Number , required : true},
+    likes : {type : Number } ,
+    price :{type : Number , required : true},
+    commentID :[{type : schema.Types.ObjectId, ref :"Comment"}] ,
+    userID :{type : schema.Types.ObjectId , ref : "User"},
+    slug: { type: String, slug: "title" }
+},{timestamps : true})
+
+var WishList = mongoose.model('WishList' , wishSchema)
+module.exports = WishList;
